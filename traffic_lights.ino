4d6fb1997a63bea42b73293dd9abbd3e525f61bd
hadeelashraf <36446976+hadeelashraf@users.noreply.github.com>
2018-02-22 21:48:38 +0200

    Add files via upload

---
+++ traffic_lights.ino
@@ -0,0 +1,22 @@
+const byte green= 13;
+const byte yellow= 12;
+const byte red= 11; 
+
+void setup() {
+    pinMode(green, OUTPUT);
+    pinMode(yellow, OUTPUT);
+    pinMode(red, OUTPUT); 
+
+}
+
+void loop() {
+    digitalWrite (green, LOW); 
+    digitalWrite (red, HIGH); 
+    delay (2000); 
+    digitalWrite (red, LOW); 
+    digitalWrite (yellow, HIGH);
+    delay (2000); 
+    digitalWrite (yellow, LOW);
+    digitalWrite (green, HIGH);
+    delay (2000);   
+}
