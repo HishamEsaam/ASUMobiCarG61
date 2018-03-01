03c1abfbd0aac39ef3f6f54f71624a5cd7c72f8f
hadeelashraf <36446976+hadeelashraf@users.noreply.github.com>
2018-03-01 22:08:57 +0200

    analog signal with arduino

---
+++ flashing_a_led_through_potentiometer.ino
@@ -0,0 +1,17 @@
+byte led = 13;
+int sensorIn = 0;  
+int sensorVal; 
+void setup() {
+  pinMode(led, OUTPUT); 
+  Serial.begin(9600); 
+
+}
+
+void loop() {
+  sensorVal = analogRead (sensorIn) / 1.023; // read value from 0 to 1000 (0 sec to 1 sec) 
+  Serial.println(sensorVal); // show the value of the sensor
+  digitalWrite (led, HIGH); 
+  delay (sensorVal);
+  digitalWrite (led, LOW); 
+  delay (sensorVal);  
+}
